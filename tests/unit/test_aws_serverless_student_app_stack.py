3f04fa8558600d0f7382c01b009efa0e61cca6a6
Amit Rai <amitrai@amits-air.home>
2023-11-08 17:32:14 -0500

    Initial commit

---
+++ tests/unit/test_aws_serverless_student_app_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from aws_serverless_student_app.aws_serverless_student_app_stack import AwsServerlessStudentAppStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in aws_serverless_student_app/aws_serverless_student_app_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = AwsServerlessStudentAppStack(app, "aws-serverless-student-app")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
